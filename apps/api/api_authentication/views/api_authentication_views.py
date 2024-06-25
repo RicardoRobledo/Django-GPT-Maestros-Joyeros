a982c1bda1d81cda5cc9595088dbed11646c8445
Ricardo_Sao <99063411+RicardoRobledo@users.noreply.github.com>
2024-06-25 00:58:11 -0600

    Update api_authentication_views.py

--- apps/api/api_authentication/views/api_authentication_views.py
+++ apps/api/api_authentication/views/api_authentication_views.py
@@ -54,5 +54,6 @@ def generate_gpt_tokens(request):
     token = token_handlers.create_token(user.first())
     print(token)
     print('-----------------------------------')
+    from django.http import JsonResponse
 
-    return Response(token, status=status.HTTP_200_OK)
+    return JsonResponse(token, status=status.HTTP_200_OK)
