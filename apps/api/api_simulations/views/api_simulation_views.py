e303f1dbab15bda4b44ea0a21a3cf510ec91fbc2
Ricardo Robledo <ricardoprogrammer44@gmail.com>
2024-07-29 00:38:08 -0600

    feat(fix): instructions for simulation path modified without numbers

--- apps/api/api_simulations/views/api_simulation_views.py
+++ apps/api/api_simulations/views/api_simulation_views.py
@@ -126,13 +126,14 @@ Ejemplo de cómo debes retroalimentar la simulación, debes de evaluar leyendo t
    *leeré toda la información de la simulación obtenida y la conversación de la simulación para poder retroalimentar*
     1.- En la conversación empezaste diciendo '¿Qué onda?' hablando de manera informal lo cual es incorrecto. Este es un ejemplo de cómo debes saludar que se menciona en la información obtenida de los procedimientos:
     Buenas tardes, te atiende tu asesor joyero José Pérez y te acompañaré en tu proceso de compra, ¿cómo te puedo ayudar? - Referencia: Información de empresa y comportamiento.
-    2.- El vendedor mencionó  al cliente que estaba dentro del plazo que se hacen devoluciones, sin embargo en la información obtenida de los procedimientos se menciona que son 14 días y no 30 - Referencia: Cambios y devoluciones.
+    2.- El vendedor mencionó  al cliente que estaba dentro del plazo que se hacen devoluciones, sin embargo en la información obtenida de los procedimientos se menciona que son (x número) de días y no (otro número) de días - Referencia: Cambios y devoluciones.
     3.- No se especifica en la información obtenida de la simulación que los anillos tengan adornos con zafiros, solo con rubíes, de igual modo mencionaste que pintamos joyas, 
     pero no hacemos pintado de joyas, solo grabado en láser - Referencia: Proceso de fabricación.
     4.- Se dijo que al final del proceso de personalización de una joya se empaca y se entrega, pero en la información obtenida de  la simulación hay un paso extra al final y es que se realiza un protocolo de posventa en el proceso final - Referencia: Personalización.
-    5.- Se mencionó que el cliente tiene un plazo de 30 días para un cambio de producto, pero la información obtenida de la simulación dice que son 40 días - Referencia: Cambios y devoluciones.
+    5.- Se mencionó que el cliente tiene un plazo de (x número) de días para un cambio de producto, pero la información obtenida de la simulación dice que son (otro número) de días - Referencia: Cambios y devoluciones.
     6.- Para la compra hiciste el proceso de generar un enlace de pago, se lo enviaste y le dijiste las promociones de productos parecidos, sin embargo el proceso es: pedir anticipo, generar enlace de pago, el cliente debe generar una solicitud y posteriormente decir las promociones de productos parecidos. Esto es el proceso que se menciona en la información obtenida de la simulación - Referencia: Proceso de ventas.
-    7.- Mencionaste que la cadena de oro Raymond es de 14 kilates, pero en la información obtenida de la simulación dice que es de 18 kilates - Referencia: Producto cadena.
+    7.- Mencionaste que la cadena de oro Raymond es de (x número) kilates, pero en la información obtenida de la simulación dice que es de (otro número) de kilates - Referencia: Producto cadena.
+    8.- Mencionaste que los datos necesarios para la factura son el nombre y teléfono, pero en la información obtenida de la simulación dice que son nombre, dirección, teléfono y correo - Referencia: Facturación.
     ... así debes de evaluar todo hasta que se cubra toda la conversación de la simulación y como se apega con la información de procedimientos.    
 """
     return Response({'instructions':inst}, content_type='application/json', status=status.HTTP_200_OK)
