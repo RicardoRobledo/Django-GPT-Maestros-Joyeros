cebe085c996ded64c51dd4271b86a561b09c22e9
Ricardo Robledo <ricardoprogrammer44@gmail.com>
2024-07-31 20:26:55 -0600

    feat(add): production commit

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,15 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'django-gpt-maestros-joyeros.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
+                 'django-gpt-maestros-joyeros.onrender.com']
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 DATABASES = {
     'default': {
